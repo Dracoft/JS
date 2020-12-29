5f192b5a6e4c4edea69e1daf5eb9783c33067413
Incógnita X <julian.cortes43@gmail.com>
2020-12-29 16:21:36 -0500

    Create Fuction.js

---
+++ Fuction.js
@@ -0,0 +1,11 @@
+fuction MyFuction () {
+    var n1 = 5;
+    var n2 = 10;
+    var resultadof = n1 + n2;
+    alert("El resultaod es: " + resultadof);
+}
+
+MyFuction();.
+MyFuction();
+MyFuction();
+MyFuction();
