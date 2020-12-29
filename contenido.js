7fe376e9b8fad4bcbeb4d5ccd359a92eabb0dc42
Incógnita X <julian.cortes43@gmail.com>
2020-12-29 15:55:11 -0500

    Update contenido.js

--- contenido.js
+++ contenido.js
@@ -1,2 +1,14 @@
 var nombre = prompt("Por favor digite su nombre");
 alert("Bienvenido al sistema," + nombre);
+/*
+var usuario = prompt("Por favor digite su Usuario");
+alert ("Verificando...");
+var password = prompt("Por favor digite su Contrasena");
+alert ("Verificando...");
+if (usuario === "Zihk0" && password === "1234"){
+     alert("Bienvenido al sistema, " + usuario);
+}
+else{
+    alert ("Credenciales Incorrectas");
+}
+*/
