67adef02702bc8d9152bccf5281a8ddc405c7693
Incógnita X <julian.cortes43@gmail.com>
2020-12-29 16:22:21 -0500

    Update Fuction.js

--- Fuction.js
+++ Fuction.js
@@ -1,11 +1,11 @@
-fuction MyFuction () {
+function MyFunction () {
     var n1 = 5;
     var n2 = 10;
     var resultadof = n1 + n2;
     alert("El resultaod es: " + resultadof);
 }
 
-MyFuction();.
-MyFuction();
-MyFuction();
-MyFuction();
+MyFunction();.
+MyFunction();
+MyFunction();
+MyFunction();
