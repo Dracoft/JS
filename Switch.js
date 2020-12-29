19cbbb21d3b6bbd70a2235ae8411a83656014e8d
Incógnita X <julian.cortes43@gmail.com>
2020-12-29 16:07:10 -0500

    Create Switch.js

---
+++ Switch.js
@@ -0,0 +1,15 @@
+var opcion = "1";
+
+switch (opcion) {
+     case "1":
+      alert ("A seleccionado la opcion uno");
+      break;
+     case "2":
+      alert ("A seleccionado la opcion dos");
+      break;
+     case "3":
+      alert ("A seleccionado la opcion tres");
+      break;
+      default:
+      alert ("Opcion invalida");
+}
