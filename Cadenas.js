b3fed5e876fa491a5ff6f804690d13572d28f347
Incógnita X <julian.cortes43@gmail.com>
2020-12-30 16:56:26 -0500

    Update Cadenas.js

--- Cadenas.js
+++ Cadenas.js
@@ -10,3 +10,10 @@ var a2 = "hola mundo";
 if ( a1.toLowerCase() == a2.toLowerCase() ) {
   alert("Son iguales");
 }//Comparación
+
+var frase = "Hola que hace dijo la cabra loca.";
+var posicion = frase.indexOf("cabra");//Buscar la posición
+
+if ( frase.indexOf("Trabajo") == -1 {
+    alert ("La palabra no se a encontrado");
+    }
