223cd07c0df0147ffbb2ee0fa80e21a59103a2c5
Incógnita X <julian.cortes43@gmail.com>
2020-12-30 13:44:06 -0500

    Update Cadenas.js

--- Cadenas.js
+++ Cadenas.js
@@ -1,2 +1,12 @@
 var texto = "Hola Mundo";
-alert ( texto.length );
+alert ( texto.length ); //Longitud de caracteres
+
+var texto = "Hola Mundo";
+alert ( texto.toUpperCase() ); //Convertir a mayusculas
+
+var a1 = "Hola Mundo";
+var a2 = "hola mundo";
+
+if ( a1.toLowerCase() == a2.toLowerCase() ) {
+  alert("Son iguales");
+}//Comparación
